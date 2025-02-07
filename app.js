a8304adc8b744301ece691a33dd1b24d1c541658
Asura <bankaikinowork@gmail.com>
2025-02-07 22:38:49 +0300

    +imdb
    now u can use not only kinopoisk, but imdb. Some styles for kinopoisk

--- app.js
+++ app.js
@@ -1,22 +1,80 @@
 let url = window.location.href;
-url  = url.replace("https://www.kinopoisk.ru/", "")
 
-let start = url.indexOf('/')
+let flag = 'imdb'
 
-let result = "https://asura-code.github.io/react-resume/#lol"
-
-for (let i=start+1; i < 20; i++) {
-    if (url[i] != "/"){
-        result+=url[i]
-    }else{
-        break
-    }
-
-    
+if (url.indexOf("kinopoisk")==-1){
+    result = imdb()
+}else{
+    result = kinopoisk()
+    flag = 'div[class*="styles_button"]'
 }
 
 
+function kinopoisk(){
+    url  = url.replace("https://www.kinopoisk.ru/", "")
+
+    let start = url.indexOf('/')
+    let result = "https://asura-code.github.io/react-resume/#lol"
+
+    for (let i=start+1; i < 20; i++) {
+        if (url[i] != "/"){
+            result+=url[i]
+        }else{
+            break
+        }
+    }
+
+    return result
+}
+
+function imdb(){
+    url  = url.replace("https://www.imdb.com/", "")
+
+    let start = url.indexOf('tt')
+    let result = "https://asura-code.github.io/react-resume/#lol"
+    
+    for (let i=start; i < 20; i++) {
+        if (url[i] != "/"){
+            result+=url[i]
+        }else{
+            break
+        }
+        
+    }
+    
+    return result
+}
+
+
+
+
+
+
 function insertText() {
+    // const newElement = document.createElement('a');
+    // newElement.textContent = "смотреть бесплатно";
+    // newElement.href = result + "#"
+    // newElement.setAttribute('target', '_blank');
+    // newElement.style.position = 'fixed';
+    // newElement.style.top = '10px';
+    // newElement.style.left = '10px';
+    // newElement.style.backgroundColor = 'white';
+    // newElement.style.padding = '10px';
+    // newElement.style.zIndex = '1000';   
+    // newElement.className = "newEl"
+    // document.body.appendChild(newElement);
+
+    // if (flag == "kinopoisk"){
+    //     const divs = document.querySelectorAll('div[class*="sc"]')
+    // }else{
+    //     const divs = document.querySelectorAll('div[class*="styles_button"]');
+    // }
+    console.log(flag)
+
+
+const divs = document.querySelectorAll(flag);
+
+if (flag == "imdb"){
     const newElement = document.createElement('a');
     newElement.textContent = "смотреть бесплатно";
     newElement.href = result + "#"
@@ -26,12 +84,42 @@ function insertText() {
     newElement.style.left = '10px';
     newElement.style.backgroundColor = 'white';
     newElement.style.padding = '10px';
-    newElement.style.zIndex = '1000';
-    
+    newElement.style.zIndex = '1000';   
+    newElement.className = "newEl"
     document.body.appendChild(newElement);
 }
 
+let counter = 0;
+divs.forEach(div => {
+    
+    const link = document.createElement('a');
+
+    link.setAttribute('target', '_blank');
+    link.textContent = "Смотреть бесплатно";
+    link.href = result + "#";
+    link.style.position = "relative";
+    link.style.alignItems = 'center';
+    link.style.justifyContent = 'center';
+    link.style.borderRadius = '5.2rem';
+    link.style.background = 'linear-gradient(135deg, #f50 69.93%, #d6bb00 100%)';
+    link.style.display = 'flex';
+    link.style.boxSizing = 'border-box'
+    link.style.color = 'white'
+    link.style.textDecoration = 'none'
+    link.style.minHeight = '5.2rem'
+    link.style.minWidth = '25rem'
+    link.style.maxHeight = '5.2rem'
+
+    if (counter == 0 ){
+    div.append(link);
+    counter +=1;
+    }
+});
+
+}
+
 insertText()
+
 
 // if (window.confirm('Хотите посмотреть эту киноленту?')) 
 //     {

c2ca35082f723fe8215f3e7d6ef5b869df0bed75
Asura <bankaikinowork@gmail.com>
2025-02-15 15:34:40 +0300

    shikimori
    +shikimori

--- app.js
+++ app.js
@@ -2,15 +2,19 @@ let url = window.location.href;
 
 let flag = 'imdb'
 
-if (url.indexOf("kinopoisk")==-1){
-    result = imdb()
-}else{
-    result = kinopoisk()
+
+if (url.includes("kinopoisk")){
+    result = kinopoisk(url)
     flag = 'div[class*="styles_button"]'
+}else if (url.includes("imdb")){
+    result = imdb()
+    flag = `imdb`
+}else if (url.includes("shikimori")){
+    flag = 'a[class*="b-link"]'
 }
 
 
-function kinopoisk(){
+function kinopoisk(url){
     url  = url.replace("https://www.kinopoisk.ru/", "")
 
     let start = url.indexOf('/')
@@ -52,9 +56,6 @@ function imdb(){
 
 function insertText() {
 
-
-const divs = document.querySelectorAll(flag);
-
 if (flag == "imdb"){
     const newElement = document.createElement('a');
     newElement.textContent = "смотреть бесплатно";
@@ -68,35 +69,75 @@ if (flag == "imdb"){
     newElement.style.zIndex = '1000';   
     newElement.className = "newEl"
     document.body.appendChild(newElement);
+} else if (flag == 'div[class*="styles_button"]'){
+    const divs = document.querySelectorAll(flag);
+
+
+    let counter = 0;
+    divs.forEach(div => {
+        const link = document.createElement('a');
+
+        link.setAttribute('target', '_blank');
+        link.textContent = "Смотреть бесплатно";
+        link.href = result + "#";
+        link.style.position = "relative";
+        link.style.alignItems = 'center';
+        link.style.justifyContent = 'center';
+        link.style.borderRadius = '5.2rem';
+        link.style.background = 'linear-gradient(135deg, #f50 69.93%, #d6bb00 100%)';
+        link.style.display = 'flex';
+        link.style.boxSizing = 'border-box'
+        link.style.color = 'white'
+        link.style.textDecoration = 'none'
+        link.style.minHeight = '5.2rem'
+        link.style.minWidth = '25rem'
+        link.style.maxHeight = '5.2rem'
+        link.style.font = `650 12pt sans-serif`
+
+        if (counter == 0 ){
+            div.append(link);
+            counter +=1;
+        }
+    });
+
+}
+else{
+    const divs = document.querySelectorAll(flag);
+    let linkHref = ''
+    let counter = 0;
+    divs.forEach(div => {
+        if(div.href.includes("www.kinopoisk")){
+            linkHref = kinopoisk(div.href)
+            const subheads = document.querySelectorAll('div[class*="c-info-right"]')
+           
+            subheads.forEach(subhead => {
+                const link = document.createElement('a');
+
+                link.setAttribute('target', '_blank');
+                link.textContent = "Смотреть бесплатно";
+                link.href = linkHref + "#";
+                link.style.position = "relative";
+                link.style.alignItems = 'center';
+                link.style.justifyContent = 'center';
+                link.style.borderRadius = '5.2rem';
+                link.style.background = 'linear-gradient(125deg,rgb(181, 115, 35) 70.93%,rgb(51, 47, 20) 90%)';
+                link.style.display = 'flex';
+                link.style.boxSizing = 'border-box'
+                link.style.color = 'white'
+                link.style.textDecoration = 'none'
+                link.style.minHeight = '3.5rem'
+                link.style.minWidth = '3.5rem'
+                link.style.maxHeight = '3.5rem'
+                link.style.font = `700 12pt sans-serif`
+                subhead.append(link);
+            });
+            
+        }
+    });
+    
+
+}
 }
 
-let counter = 0;
-divs.forEach(div => {
-
-    const link = document.createElement('a');
-
-    link.setAttribute('target', '_blank');
-    link.textContent = "Смотреть бесплатно";
-    link.href = result + "#";
-    link.style.position = "relative";
-    link.style.alignItems = 'center';
-    link.style.justifyContent = 'center';
-    link.style.borderRadius = '5.2rem';
-    link.style.background = 'linear-gradient(135deg, #f50 69.93%, #d6bb00 100%)';
-    link.style.display = 'flex';
-    link.style.boxSizing = 'border-box'
-    link.style.color = 'white'
-    link.style.textDecoration = 'none'
-    link.style.minHeight = '5.2rem'
-    link.style.minWidth = '25rem'
-    link.style.maxHeight = '5.2rem'
-
-    if (counter == 0 ){
-        div.append(link);
-        counter +=1;
-    }
-});
-
-}
 
 insertText()
